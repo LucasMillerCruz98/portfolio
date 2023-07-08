a8ea39cd69b6ca0ce22197b4ab9f375923e8436f
LucasMillerCruz98 <lucas.miller.s.cruz1998@gmail.com>
2023-07-08 01:38:19 -0300

    Change code

--- components/photo/MyPhoto.js
+++ components/photo/MyPhoto.js
@@ -1,36 +1,73 @@
 class MyPhoto extends HTMLElement {
   constructor() {
     super()
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot.innerHTML = `
-      <style>
-        .program-section-right figure img {
-          width: 44rem;
-          border-radius: 2rem;
-        }
+    
+    this.build()
+  }
 
-        .program-section-right figure {
-          margin-bottom: 0;
-        }
-        
-        @media(max-width: 1097px) {
-        
-          .program-section-right figure img {
-            width: 37rem;
-          }
-        
-          .program-section-right figure {
-            margin: 1rem 0;
-          }
-        }
-      </style>
+  build() {
+    const shadow = this.attachShadow({ mode: 'open' })
+
+    shadow.appendChild(this.styles())
+
+    const img = this.createImg()
+    const figure = this.createFigure()
+    const sectionPhoto = this.createContainer()
+    
+    shadow.appendChild(sectionPhoto)
+    shadow.appendChild(figure)
+    sectionPhoto.appendChild(figure)
+    figure.appendChild(img)
+
+    return shadow
+  }
+
+  createContainer() {
+    const section = document.createElement('section')
+    section.classList.add('program-section-right')
+
+    return section
+  }
+
+  createImg() {
+    const img = document.createElement('img')
+    img.setAttribute('src', './images/icon-web.png')
+    img.setAttribute('alt', 'Minha foto')
+    
+    return img
+  }
+
+  createFigure() {
+    const figure = document.createElement('figure')
+
+    return figure
+  }
+  
+  styles() {
+    const style = document.createElement('style')
+    style.textContent = `
+      .program-section-right figure img {
+        width: 44rem;
+        border-radius: 2rem;
+      }
+
+      .program-section-right figure {
+        margin-bottom: 0;
+      }
       
-      <section class="program-section-right">
-        <figure>
-          <img src="./images/icon-web.png" alt="Minha foto">
-        </figure>
-      </section>
+      @media(max-width: 1097px) {
+      
+        .program-section-right figure img {
+          width: 37rem;
+        }
+      
+        .program-section-right figure {
+          margin: 1rem 0;
+        }
+      }
     `
+
+    return style
   }
 }
 
